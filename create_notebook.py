3c709ec6d7bd051600233dc265ce196e084c6d20
Kate Hess <caitlin.h.hess@gmail.com>
2017-07-05 13:11:29 -0700

    First commit

---
+++ create_notebook.py
@@ -0,0 +1,98 @@
+import nbformat as nbf
+from subprocess import call
+
+nb = nbf.v4.new_notebook()
+
+text = """\
+# My first automatic Jupyter Notebook
+This is an auto-generated notebook."""
+
+code = """\
+%pylab inline
+hist(normal(size=2000), bins=50);"""
+
+styles = '''
+Emphasis, aka italics, with *asterisks* or _underscores_.
+
+Strong emphasis, aka bold, with **asterisks** or __underscores__.
+
+Combined emphasis with **asterisks and _underscores_**.
+
+Strikethrough uses two tildes. ~~Scratch this.~~
+'''
+
+table = '''
+Colons can be used to align columns.
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
+There must be at least 3 dashes separating each header cell.
+The outer pipes (|) are optional, and you don't need to make the
+raw Markdown line up prettily. You can also use inline Markdown.
+
+Markdown | Less | Pretty
+--- | --- | ---
+*Still* | `renders` | **nicely**
+1 | 2 | 3
+'''
+
+image = '''
+Here's our logo (hover to see the title text):
+
+Inline-style:
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
+
+Reference-style:
+![alt text][logo]
+
+[logo]: https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 2"
+'''
+
+links = '''
+[I'm an inline-style link](https://www.google.com)
+
+[I'm an inline-style link with title](https://www.google.com "Google's Homepage")
+
+[I'm a reference-style link][Arbitrary case-insensitive reference text]
+
+[I'm a relative reference to a repository file](../blob/master/LICENSE)
+
+[You can use numbers for reference-style link definitions][1]
+
+Or leave it empty and use the [link text itself].
+
+URLs and URLs in angle brackets will automatically get turned into links.
+http://www.example.com or <http://www.example.com> and sometimes
+example.com (but not on Github, for example).
+
+Some text to show that the reference links can follow later.
+
+[arbitrary case-insensitive reference text]: https://www.mozilla.org
+[1]: http://slashdot.org
+[link text itself]: http://www.reddit.com
+'''
+
+math = '''
+$$f(x) = \frac{xy}{z}$$
+'''
+
+nb['cells'] = [nbf.v4.new_markdown_cell(text),
+               nbf.v4.new_code_cell(code),
+               nbf.v4.new_markdown_cell(styles),
+               nbf.v4.new_markdown_cell(table),
+               nbf.v4.new_markdown_cell(image),
+               nbf.v4.new_markdown_cell(links),
+               nbf.v4.new_markdown_cell(math)
+               ]
+
+fname = 'test.ipynb'
+
+with open(fname, 'w') as f:
+    nbf.write(nb, f)
+
+# ipython nbconvert --to FORMAT notebook.ipynb
+call(["ipython", "nbconvert", "--to", "html", "test.ipynb"])
